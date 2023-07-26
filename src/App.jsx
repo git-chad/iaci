41a89e74e94c3da4a3f658bd52d004df1d0fb5fd
git-chad <tobimocc@gmail.com>
2023-07-26 18:57:25 -0300

    upload imgs test

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,1 @@
-import './App.css'
 
-function App() {
-  return (
-    <>
-      <div>
-        <h1 className='bg-red-500 p-5 rounded-xl'>Hola tronco</h1>
-      </div>
-    </>
-  )
-}
-
-export default App
