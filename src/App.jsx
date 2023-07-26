22c006dac7665990fd5da128271c76ad46769363
git-chad <tobimocc@gmail.com>
2023-07-26 17:16:25 -0300

    first commmit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className='bg-red-500 p-5 rounded-xl'>Hola tronco</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
