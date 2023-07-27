4c94331a9137c9f1455850451d37ebbc8fd6e64b
git-chad <tobimocc@gmail.com>
2023-07-27 12:36:47 -0300

    added custom fonts - ork class and ppa class

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,12 +1,23 @@
-import Navbar from '../components/Navbar'
+import Navbar from "../components/Navbar";
+import Shape01 from "../assets/images/shape01.png";
+import { motion } from "framer-motion"
 
-const hero = () => {
+const Hero = () => {
   return (
     <div>
-        <Navbar/>
-        <div className='w-screen h-screen bg-[#F5F5F5]'></div>
+      <Navbar />
+      <div className="relative w-screen h-screen bg-[#F5F5F5]">
+        <motion.img
+          initial={{ opacity: 0, scale: 0, x: 200, }}
+          animate={{ opacity: 1, scale: 1, x: 0 }}
+          transition={{ duration: 1 }}
+          className="fixed -right-80 h-full z-10"
+          src={Shape01}
+          alt="Shape01"
+        ></motion.img>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default hero
+export default Hero;
