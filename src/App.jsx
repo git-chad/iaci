093af0365b68df5b413f6ee263b09cd27dc5a50b
Troncoso Juan <troncosojuanbernabe@gmail.com>
2023-07-27 23:27:44 -0300

    Add page-TestForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Hero from './pages/hero'
+import EnglishLevelPage from './pages/TestForm'
 
 function App() {
   return (
     <>
       <div>
         <Hero/>
+        <EnglishLevelPage/>
       </div>
     </>
   );
---
+++ src/pages/TestForm.jsx
@@ -0,0 +1,23 @@
+
+const EnglishLevelPage = () => {
+  return (
+    <div className="grid place-items-center h-screen bg-gray-100">
+      <div className="max-w-lg text-center flex flex-col justify-center items-center">
+        <h1 className="text-5xl font-bold mb-6 ork">
+          Want to find out your<br />
+          English level?
+        </h1>
+        <p className="text-xl mb-6 max-w-[370px] text-left ppa">
+          By having a better understanding of your
+          English level, we ll be able to
+          recommend the perfect course to you
+        </p>
+        <a href="#" className="text-black hover:underline ppa"> 
+          Test my skills
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default EnglishLevelPage;
