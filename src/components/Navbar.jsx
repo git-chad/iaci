f8d166aac84baf0a8ac4e4874ff66c251e991a0e
git-chad <tobimocc@gmail.com>
2023-08-01 11:49:26 -0300

    added placeholder logo, change navbar position, added fallback fonts

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import { motion } from "framer-motion";
+import LogoSmall from "../assets/images/LogoSmall.svg";
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-transparent p-8 z-50">
+    <nav className="absolute top-0 left-0 w-full p-8 z-50">
       <div className="max-w-5xl mx-auto flex items-center justify-between">
-        <h1 className="ork text-2xl font-bold">IACI</h1>
+        <div className="logo-container">
+          <img src={LogoSmall}></img>
+          <h1 className="ork text-2xl font-bold">IACI</h1>
+        </div>
         <ul className="ppa flex space-x-10 text-2xl font-semibold cursor-pointer">
           <motion.li whileHover={{ y: -2 }} transition={{ duration: 0.15 }}>
             Courses
