994bf10e7432e7388c3cf53006464c93a8bc5d76
Troncoso Juan <troncosojuanbernabe@gmail.com>
2023-08-07 17:55:49 -0300

    Fix Footer 01

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
       <div className="max-w-6xl mx-auto px-4 py-8 flex flex-wrap justify-between">
         <div className="w-full md:w-1/3 mb-6 md:mb-0">
           <h3 className="text-lg font-bold mb-2">Contact</h3>
-          <p>info@iaciweb.com.ar</p>
+          <p>AGREGAR EMAIL</p>
           <p>+5411 4256-9819</p>
           <p>Calle 149 No1354, (1884) Berazategui, Buenos Aires - Argentina</p>
         </div>
@@ -18,11 +18,20 @@ const Footer = () => {
             <li><a href="#">Contact</a></li>
           </ul>
         </div>
-        <div className="w-full md:w-1/3 flex justify-end">
-          <a href="#" className="mr-4">Facebook</a>
-          <a href="#" className="mr-4">Instagram</a>
-          <a href="#" className="mr-4">WhatsApp</a>
+        <div className="w-full md:w-1/3 flex justify-end justify-center items-center">
+          <a href="#" className="mr-4 text-3xl hover:hover:text-blue-300 transition-colors">
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a href="#" className="mr-4 text-3xl hover:text-red-300 transition-colors">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="#" className="mr-4 text-3xl hover:text-green-200 transition-colors">
+            <i className="fab fa-whatsapp"></i>
+          </a>
         </div>
+      </div>
+      <div className="max-w-6xl mx-auto px-auto py-auto text-center text-gray-400">
+        <p className="text-sm">© 2023 IACI. All rights reserved.</p>
       </div>
     </footer>
   );
